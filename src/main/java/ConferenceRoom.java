4c2a3522249e5eb1e313c208d7e768a109c85549
yulirunshao <382316736@qq.com>
2020-01-08 11:59:12 +0000

    bedroom class constructed

--- src/main/java/ConferenceRoom.java
+++ src/main/java/ConferenceRoom.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+
 public class ConferenceRoom {
+    private int capacity;
+    private ArrayList<Guest> guests;
+    private String name;
+
+    public ConferenceRoom(int capacity, String name){
+        this.capacity = capacity;
+        this.name = name;
+        this.guests = new ArrayList<Guest>();
+    }
 }
--- src/test/java/ConferenceRoomTest.java
+++ src/test/java/ConferenceRoomTest.java
@@ -1,2 +1,12 @@
+import org.junit.Before;
+
 public class ConferenceRoomTest {
+    private  ConferenceRoom conferenceRoom1;
+    private  ConferenceRoom conferenceRoom2;
+
+    @Before
+    public void before(){
+        conferenceRoom1 = new ConferenceRoom(6,"Chocolate");
+        conferenceRoom2 = new ConferenceRoom(8,"Gingerbread");
+    }
 }
